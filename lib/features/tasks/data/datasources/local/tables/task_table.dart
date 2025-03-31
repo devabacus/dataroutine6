00797463ec4ea90ee5afb524d423f5d02914de81
mrfrolk <ekaterita20@gmail.com>
2025-03-31 16:53:29 +0500

    1

---
+++ lib/features/tasks/data/datasources/local/tables/task_table.dart
@@ -0,0 +1,18 @@
+
+
+import 'package:dataroutine6/features/tasks/data/datasources/local/tables/category_table.dart';
+import 'package:drift/drift.dart';
+
+class TaskTable extends Table {
+
+  IntColumn get id => integer().autoIncrement()();
+  TextColumn get title => text()();
+  TextColumn get description => text()();
+  IntColumn get duration => integer()();
+  DateTimeColumn get createdAt => dateTime()();
+  DateTimeColumn get dueDateTime => dateTime()();
+  IntColumn get categoryId => integer().references(CategoryTable, #id)();
+
+}
+
+
