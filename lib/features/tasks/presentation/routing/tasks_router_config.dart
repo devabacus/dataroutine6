4975e9fc8fb70de4f4af20a11dc0be0a9c069116
mrfrolk <ekaterita20@gmail.com>
2025-04-01 08:48:50 +0500

    1

--- lib/features/tasks/presentation/routing/tasks_router_config.dart
+++ lib/features/tasks/presentation/routing/tasks_router_config.dart
@@ -6,7 +6,6 @@ import 'package:go_router/go_router.dart';
 import '../../presentation/pages/category/upsert_category_page.dart';
 import '../../presentation/pages/category/view_category_page.dart';
 import '../../presentation/pages/tag/view_tag_page.dart';
-import '../../presentation/pages/task/add_task_page.dart';
 import '../../presentation/pages/task/view_task_page.dart';
 import '../../presentation/pages/tasks_page.dart';
 import '../pages/tag/upsert_tag_page.dart';
