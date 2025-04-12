7703e3fd4124774febba1e088d7a07246ac811f3
mrfrolk <ekaterita20@gmail.com>
2025-04-12 05:16:42 +0500

    1

--- lib/features/tasks/presentation/pages/task/upsert_task_page.dart
+++ lib/features/tasks/presentation/pages/task/upsert_task_page.dart
@@ -1,73 +1,79 @@
-import 'package:dataroutine6/core/utils/date_time_picker_utils.dart';
-import 'package:dataroutine6/features/tasks/presentation/pages/task/widgets/tag_section_widget.dart';
-import 'package:dataroutine6/features/tasks/presentation/pages/task/widgets/task_form_controllers.dart';
-import 'package:dataroutine6/features/tasks/presentation/providers/category/category_by_id_provider.dart';
-import 'package:dataroutine6/features/tasks/presentation/providers/task/task_selected_provider.dart';
-import 'package:dataroutine6/features/tasks/presentation/providers/task/task_state_providers.dart';
-import 'package:dataroutine6/features/tasks/presentation/routing/tasks_routes_constants.dart';
+import 'package:dataroutine6/features/tasks/domain/entities/task/task.dart';
+import 'package:dataroutine6/features/tasks/presentation/common_widgets/form_controller_mixin.dart';
+import 'package:dataroutine6/features/tasks/presentation/common_widgets/upsert_page_base.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:ui_kit/ui_kit.dart';
 
-import '../../../domain/entities/task/task.dart';
+import '../../../../../core/utils/date_time_picker_utils.dart';
+import '../../providers/category/category_by_id_provider.dart';
+import '../../providers/task/task_selected_provider.dart';
+import '../../providers/task/task_state_providers.dart';
+import '../../routing/tasks_routes_constants.dart';
+import 'widgets/tag_section_widget.dart';
+import 'widgets/task_form_controllers.dart';
 
-class UpsertTaskPage extends ConsumerStatefulWidget {
+class UpsertTaskPage extends BaseUpsertPage<TaskEntity> {
   final bool isEditing;
 
   const UpsertTaskPage({this.isEditing = false, super.key});
 
   @override
-  ConsumerState<ConsumerStatefulWidget> createState() => UpdateTaskPageState();
+  ConsumerState<BaseUpsertPage<TaskEntity>> createState() =>
+      _UpsertTaskPageState();
 }
 
-class UpdateTaskPageState extends ConsumerState<UpsertTaskPage> {
+class _UpsertTaskPageState
+    extends BaseUpsertPageState<TaskEntity, UpsertTaskPage>
+    with FormControllersMixin<TaskEntity> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   DateTime _selectedDateTime = DateTime.now();
 
   late final TaskFormControllers ctrl;
   TaskEntity? currentEntity;
   bool isInitialized = false;
+  Task? taskContr;
+
 
   int? taskId;
   int? catId;
 
   @override
   void initState() {
-    super.initState();
     ctrl = TaskFormControllers();
-
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      if (widget.isEditing) {
-        final selectedTask = ref.read(selectedTaskProvider)!;
-        ctrl.title.text = selectedTask.title;
-        ctrl.description.text = selectedTask.description;
-
-        catId = selectedTask.categoryId;
-        ctrl.categoryId.text = catId.toString();
-
-        _selectedDateTime = selectedTask.dueDateTime;
-        ctrl.dueDateTime.text = DateTimePickerUtils.formatDateTime(
-          _selectedDateTime,
-        );
-
-        ctrl.duration.text = selectedTask.duration.toString();
-        taskId = selectedTask.id;
-        getCurrentCategory();
-        setState(() {
-          isInitialized = true;
-        });
-      } else {
-        ctrl.dueDateTime.text = DateTimePickerUtils.formatDateTime(
-          _selectedDateTime,
-        );
-      }
-    });
+    super.initState();
   }
 
+
   @override
-  void dispose() {
-    ctrl.dispose();
-    super.dispose();
+  void initializeData() {
+    
+    if (widget.isEditing) {
+      final selectedTask = ref.read(selectedTaskProvider)!;
+      ctrl.title.text = selectedTask.title;
+      ctrl.description.text = selectedTask.description;
+
+      catId = selectedTask.categoryId;
+      ctrl.categoryId.text = catId.toString();
+
+      _selectedDateTime = selectedTask.dueDateTime;
+      ctrl.dueDateTime.text = DateTimePickerUtils.formatDateTime(
+        _selectedDateTime,
+      );
+
+      ctrl.duration.text = selectedTask.duration.toString();
+      taskId = selectedTask.id;
+      getCurrentCategory();
+      setState(() {
+        isInitialized = true;
+      });
+    } else {
+      ctrl.dueDateTime.text = DateTimePickerUtils.formatDateTime(
+        _selectedDateTime,
+      );
+    }
   }
 
   Future<void> getCurrentCategory() async {
@@ -78,25 +84,34 @@ class UpdateTaskPageState extends ConsumerState<UpsertTaskPage> {
   }
 
   @override
-  Widget build(BuildContext context) {
-    final taskContr = ref.read(taskProvider.notifier);
-    final selectedTaskContr = ref.read(selectedTaskProvider.notifier);
+  void navigateBack() {
+    context.goNamed(TasksRoutes.viewTask);
+  }
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          widget.isEditing ? "Редактирование задачи" : "Добавить задачу",
-        ),
-        leading: IconButton(
-          onPressed: () {
-            context.goNamed(TasksRoutes.viewTask);
-          },
-          icon: Icon(Icons.arrow_back),
-        ),
+  @override
+  AppBar buildAppBar() {
+    return AppBar(
+      title: Text(
+        widget.isEditing ? "Редактирование задачи" : "Добавить задачу",
       ),
-      body: Column(
+      leading: IconButton(
+        onPressed: () {
+          context.goNamed(TasksRoutes.viewTask);
+        },
+        icon: Icon(Icons.arrow_back),
+      ),
+    );
+  }
+
+  @override
+  Widget buildForm() {
+    taskContr = ref.read(taskProvider.notifier);
+    final selectedTaskContr = ref.read(selectedTaskProvider.notifier);
+    return Form(
+      key: _formKey,
+      child: Column(
         children: [
-          TextFieldFactory.listTile(ctrl.title, hint: "Название"),
+          TextFieldFactory.listTile(ctrl.title, hint: "Названиеasdf"),
           TextFieldFactory.listTile(ctrl.description, hint: "Описание"),
           TextFieldFactory.listTile(ctrl.duration, hint: "Длительность"),
           TextFieldFactory.listTile(
@@ -124,7 +139,7 @@ class UpdateTaskPageState extends ConsumerState<UpsertTaskPage> {
                       _selectedDateTime,
                     ) ??
                     DateTime.now();
-
+      
                 ctrl.dueDateTime.text = DateTimePickerUtils.formatDateTime(
                   _selectedDateTime,
                 );
@@ -132,7 +147,7 @@ class UpdateTaskPageState extends ConsumerState<UpsertTaskPage> {
               icon: Icon(Icons.date_range),
             ),
           ),
-
+      
           TextFieldFactory.listTile(
             ctrl.categoryId,
             hint: "Выбрать категорию",
@@ -144,13 +159,20 @@ class UpdateTaskPageState extends ConsumerState<UpsertTaskPage> {
           ElevatedButton(
             style: AppButtonStyle.basicStyle,
             onPressed: () {
-              _saveTask(taskContr);
+              _saveTask(taskContr!);
             },
             child: Text("Сохранить"),
           ),
         ],
       ),
     );
+  }
+
+  @override
+  void saveEntity() {
+    if (!_formKey.currentState!.validate()) return;
+    _saveTask(taskContr!);
+    
   }
 
   Widget _pickCategory(SelectedTask selectedTaskContr) {
